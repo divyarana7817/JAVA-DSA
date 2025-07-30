0af74d31ac45253195792d4841147b05a2834976
Divya <divyarana7817@gmail.com>
2025-07-30 21:49:09 +0530

    Updated sorting codes

--- Sorting/Bubble Sort.java
+++ Sorting/Bubble Sort.java
@@ -1,15 +1,25 @@
 
 class Solution {
     public int[] bubbleSort(int[] nums) {
-  for(int i =0; i<nums.length-1; i++){
-    for(int j=0; j<nums.length-1; j++){
-        if(nums[j] > nums[j+1]){
-            int temp = nums[j];
-            nums[j] = nums[j+1];
-            nums[j+1] = temp;
+        int n = nums.length;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int swap = 0;
+
+            for (int j = 0; j <= i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    // Swap
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                    swap = 1;
+                }
+            }
+
+            // No swaps = already sorted
+            if (swap == 0) break;
         }
-    }
-  }
-  return nums;
+
+        return nums;
     }
 }
--- Sorting/Selection Sort.java
+++ Sorting/Selection Sort.java
@@ -1,9 +1,10 @@
 
 class Solution {
     public int[] selectionSort(int[] nums) {
-        for (int i = 0; i < nums.length - 1; i++) {
+        int n = nums.length;
+        for (int i = 0; i <= n - 2; i++) {
             int small = i;
-            for (int j = i + 1; j < nums.length; j++) {
+            for (int j = i + 1; j <= n-1; j++) {
                 if (nums[small] > nums[j]) {
                     small = j;
                 }
