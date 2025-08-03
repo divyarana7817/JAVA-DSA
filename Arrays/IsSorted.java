d41c527ffe9e281120e31d1747a3d0e29d81d5c3
Divya <divyarana7817@gmail.com>
2025-08-03 23:54:32 +0530

    Arrays

---
+++ Arrays/IsSorted.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+class Solution {
+    public boolean isSorted(int[] arr) {
+        // code here
+        int n = arr.length;
+        for(int i=1; i<n; i++){
+            if(arr[i] < arr[i-1]){
+                return false; 
+            }
+        }
+        return true;
+    }
+}
