8ed7a0571df03fbb8c9fb86a23bb729fe94bbc21
Divya <divyarana7817@gmail.com>
2025-08-03 22:19:09 +0530

    Add: method to find second largest element

---
+++ Arrays/SecondLargest.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        // code here
+        int n = arr.length;
+        int largest = arr[0];
+        int slargest = -1;
+        for(int i=0; i<n; i++){
+            if(arr[i] > largest){
+                slargest = largest;
+                largest = arr[i];
+            }
+            else if(arr[i] < largest && arr[i] > slargest){
+                slargest = arr[i];
+            }
+        }
+        return slargest;
+    }
+}
